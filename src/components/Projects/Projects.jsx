5b1ae7b82aa402ad4830fdb7a55179077f35ea49
imbilgates <bilgates270703@gmail.com>
2025-06-29 17:41:48 +0530

    Refactor Projects component for improved readability

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,10 +2,10 @@ import uniqid from 'uniqid'
 import { projects } from '../../portfolio'
 import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
-
 const Projects = () => {
   if (!projects.length) return null
 
+  
   return (
     <section id='projects' className='section projects'>
       <h2 className='section__title'>Projects</h2>
